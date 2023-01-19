9e83465568544f5d8dc513ff8236b39a508abee3
Daniel <tishakovdanyil@gmail.com>
2023-01-19 07:50:40 +0000

    craeta def photo

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+import telebot
+
+bot = telebot.TeleBot("5633349657:AAHvXGphY7k4i-TSoGE23RMInvkYlnUxhYg")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f"Привет!, <b>{message.from_user.first_name} <u>{message.from_user.username}.</u></b>"
+    bot.send_message(message.chat.id, mess, parse_mode="html")
+
+
+@bot.message_handler(content_types=["text"])
+def get_user_text(message):
+    if message.text == "Hello" or message.text == "Hi":
+        bot.send_message(message.chat.id, "Welcome to chatbot", parse_mode="html")
+    elif message.text == "photo":
+        photo = open("lamp.jpg", "rb")
+        bot.send_photo(message.chat.id, photo)
+    elif message.text == "id":
+        bot.send_message(message.chat.id, f"Your ID:{message.from_user.id}""Welcome to chatbot", parse_mode="html")
+    else:
+        bot.send_message(message.chat.id, "I no understand you", parse_mode="html")
+
+
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, "nice foto")
+
+@bot.message_handler(commands=["website"])
+def website(message):
+    bot.send_message()
+
+bot.polling(none_stop=True)
+
+if __name__ == "__main__":
+    start()
