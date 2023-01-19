53acd87a133cd05af099c8bf0b1bd9d2664d97be
Daniel <tishakovdanyil@gmail.com>
2023-01-19 18:25:20 +0000

    init def website

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot("5633349657:AAHvXGphY7k4i-TSoGE23RMInvkYlnUxhYg")
 
@@ -11,17 +10,17 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode="html")
 
 
-@bot.message_handler(content_types=["text"])
-def get_user_text(message):
-    if message.text == "Hello" or message.text == "Hi":
-        bot.send_message(message.chat.id, "Welcome to chatbot", parse_mode="html")
-    elif message.text == "photo":
-        photo = open("lamp.jpg", "rb")
-        bot.send_photo(message.chat.id, photo)
-    elif message.text == "id":
-        bot.send_message(message.chat.id, f"Your ID:{message.from_user.id}""Welcome to chatbot", parse_mode="html")
-    else:
-        bot.send_message(message.chat.id, "I no understand you", parse_mode="html")
+# @bot.message_handler(content_types=["text"])
+# def get_user_text(message):
+#     if message.text == "Hello" or message.text == "Hi":
+#         bot.send_message(message.chat.id, "Welcome to chatbot", parse_mode="html")
+#     elif message.text == "photo":
+#         photo = open("lamp.jpg", "rb")
+#         bot.send_photo(message.chat.id, photo)
+#     elif message.text == "id":
+#         bot.send_message(message.chat.id, f"Your ID:{message.from_user.id}""Welcome to chatbot", parse_mode="html")
+#     else:
+#         bot.send_message(message.chat.id, "I no understand you", parse_mode="html")
 
 
 @bot.message_handler(content_types=['photo'])
@@ -30,7 +29,9 @@ def get_user_photo(message):
 
 @bot.message_handler(commands=["website"])
 def website(message):
-    bot.send_message()
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Go to site:", url = "http://habr.com"))
+    bot.send_message(message.chat.id, "visit this site", reply_markup=markup)
 
 bot.polling(none_stop=True)
 
